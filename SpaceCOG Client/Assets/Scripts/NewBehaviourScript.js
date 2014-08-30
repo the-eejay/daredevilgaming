b1f86786acfe450106a6ef631908f87d47b82954
Geiun <alan.murtagh@gmail.com>
2014-08-30 15:12:52 +1000

    Globe spins & camera moves.
    Just started learning how to script in Unity. The Planet in the main
    menu screen now rotates slowly, and when the user presses 'Play', the
    camera moves to a different location (no new main menu yet though).

---
+++ SpaceCOG Client/Assets/Scripts/NewBehaviourScript.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+
+}
+
+function OnGUI () {
+	if (Event.current.type == EventType.MouseDown) {
+		MenuCameraRails();
+	}
+}
+
+function MenuCameraRails () {
+	for(var i = 0; i < 20; ++i) {
+		Camera.main.transform.Translate(Vector3(-0.2, 0.2, 0));
+		yield WaitForSeconds(0.001);
+	}
+}
