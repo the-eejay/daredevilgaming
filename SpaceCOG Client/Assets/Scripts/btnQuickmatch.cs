7f2f0af9d61e2f699646cadbb805e3a38c7cdd28
Geiun <alan.murtagh@gmail.com>
2014-09-01 08:56:07 +1000

    Added new scene for game prototype
    Includes a change to build settings, as well as a script to begin the
    game when "Quickmatch" is pressed. Committed now to avoid conflicts in
    future.

---
+++ SpaceCOG Client/Assets/Scripts/btnQuickmatch.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class btnQuickmatch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Prototype() {
+		Application.LoadLevel ("GameScene");
+	}
+}
