eb346190662e1c57b707ea75061c411c28e3f0cd
Geiun <alan.murtagh@gmail.com>
2014-09-01 12:12:24 +1000

    Ships shoots & scoots.
    Player controllable ships is now able to move with 'wasd', and will
    shoot a projectile towards the cursor whenever the left mouse button is
    clicked.

--- SpaceCOG Client/Assets/Scripts/playerController.cs
+++ SpaceCOG Client/Assets/Scripts/playerController.cs
@@ -5,20 +5,37 @@ public class playerController : MonoBehaviour {
 
 	float motionSpeed = 100.0f;
 	float m45Speed = 70.7f;
+	float bulletSpeed = 10000.0f;
+
+	GameObject bullet;
+	Plane targettingPlane;
 
 	// Use this for initialization
 	void Start () {
-	
+		bullet = (GameObject)Resources.Load ("prefabBullet");
+		targettingPlane = new Plane (new Vector3(0,0,1), new Vector3(0,0,0));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Move ();
+		Shoot ();
 	}
 
 	void Shoot() {
 		if (Input.GetMouseButtonDown(0)) {
+
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			float dist = 2000.0f;
+			if (targettingPlane.Raycast(ray, out dist)) {
+				Vector3 targetCoordinates = ray.GetPoint(dist);
+				Vector3 vel = targetCoordinates - this.transform.position;
+				vel.Normalize();
+				vel = vel * bulletSpeed;
+				GameObject tmp = (GameObject)Instantiate(bullet);
+				tmp.transform.position = this.transform.position;
+				tmp.rigidbody.AddForce(vel);
+			}
 		}
 	}
 
