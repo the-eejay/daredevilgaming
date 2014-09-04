6ed19a4afdb92255f8ee6feb18b3f6b61c3b0e42
rayrayj92 <rayrayj92@gmail.com>
2014-09-04 20:10:59 +1000

    Added a basic scene for the login page with GUI.

--- SpaceCOG Client/Assets/Scripts/Login/login.cs
+++ SpaceCOG Client/Assets/Scripts/Login/login.cs
@@ -3,15 +3,15 @@ using System.Collections;
 
 public class login : MonoBehaviour {
 	//public variables
-	public string CurrentMenu;
+	public string CurrentMenu = "Login";
 
 	//Static variables
-	public static string email = "";
+	public static string username = "";
 	public static string password = "";
 
 	//private variables
-	private string CreateAccountLink = "";
-	private string LoginLink = "";
+	//private string CreateAccountLink = "";
+	//private string LoginLink = "";
 
 	//GUI variables
 	public float X;
@@ -25,17 +25,48 @@ public class login : MonoBehaviour {
 	}
 	
 	// Main GUI Function
-	void GUIwindow(){
+	void OnGUI(){
+
+		if (CurrentMenu == "Login") 
+		{
+			LoginGUI();
+		}
+		else if (CurrentMenu == "CreateAccount")
+		{
+			CreateAccountGUI();
+		}
 
 	}
 
 	// GUI for creating the account.
 	void CreateAccountGUI() {
+		GUI.Box(new Rect(374, 47, 730, 526), "Create Account");
+		
+		if (GUI.Button (new Rect (504, 503, 160, 27), "Done")) {
+			
+		}
+		if (GUI.Button (new Rect (777,503,160,27), "Cancel")) {
+			CurrentMenu = "Login";
+		}
 
 	}
 
 	// Main Login GUI
 	void LoginGUI() {
+		GUI.Box(new Rect(374, 47, 730, 526), "Login");
+
+		if (GUI.Button (new Rect (504, 503, 160, 27), "Create Account")) {
+			//CurrentMenu = "CreateAccount";
+			//DO Nothing for now
+		}
+		if (GUI.Button (new Rect (777,503,160,27), "Log In")) {
+
+		}
+		GUI.Label (new Rect(513, 173, 80, 20), "Username:");
+		username = GUI.TextField (new Rect(513, 207, 440, 23), username);
+
+		GUI.Label (new Rect(513, 255, 80, 20), "Password:");
+		password = GUI.TextField (new Rect(513, 287, 440, 23), password);
 
 	}
 
