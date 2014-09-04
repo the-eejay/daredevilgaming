dea5c8eaeca883017161fb95ea017e1c231cc387
rayrayj92 <rayrayj92@gmail.com>
2014-09-04 15:05:35 +1000

    added some variables and methods. (login.cs)

---
+++ SpaceCOG Client/Assets/Scripts/Login/login.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class login : MonoBehaviour {
+	//public variables
+	public string CurrentMenu;
+
+	//Static variables
+	public static string email = "";
+	public static string password = "";
+
+	//private variables
+	private string CreateAccountLink = "";
+	private string LoginLink = "";
+
+	//GUI variables
+	public float X;
+	public float Y;
+	public float Width;
+	public float Height;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Main GUI Function
+	void GUIwindow(){
+
+	}
+
+	// GUI for creating the account.
+	void CreateAccountGUI() {
+
+	}
+
+	// Main Login GUI
+	void LoginGUI() {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
