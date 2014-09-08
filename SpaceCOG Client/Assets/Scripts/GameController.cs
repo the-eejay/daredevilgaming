3b45aac7e6b6d9b6dbc7621b052736606b46cb62
Ethan Christie <the.eejay@gmail.com>
2014-09-08 21:25:15 +1000

    It's a real game! Sort of...
    Asteroids spawn near player.  Player scores points for shooting
    asteroids and dies if he is hit.

---
+++ SpaceCOG Client/Assets/Scripts/GameController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	private int score;
+
+	void Start() {
+		StartCoroutine(SpawnWaves());
+		score = 0;
+	}
+
+	IEnumerator SpawnWaves() {
+
+		yield return new WaitForSeconds(startWait);
+		while (true) {
+			for (int i = 0; i < hazardCount; i++) {
+				Vector3 playerPosition = GameObject.Find ("PlayerSphere").transform.position;
+				float randomX = Random.Range(-50, 50);
+				float randomY = Random.Range(-50, 50);
+
+				if (randomX < 0) {
+					randomX = Mathf.Min(randomX, -10);
+				} else {
+					randomX = Mathf.Max(randomX, 10);
+				}
+
+				if (randomY < 0) {
+					randomY = Mathf.Min(randomY, -10);
+				} else {
+					randomY = Mathf.Max(randomY, 10);
+				}
+
+				Vector3 spawnPosition = new Vector3(playerPosition.x + randomX, playerPosition.y + randomY, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+		}
+	}
+
+	void onGUI() {
+		GUI.Label (new Rect (10, 10, 100, 20), "Score: " + score);
+	}
+
+	public void AddScore(int scoreValue) {
+		score += scoreValue;
+	}
+
+	public int GetScore() {
+		return this.score;
+	}
+}
---
+++ SpaceCOG Client/Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 targetPos = GameObject.Find("PlayerSphere").transform.position;
+		rigidbody.velocity = (targetPos - this.transform.position).normalized * speed;
+	}
+}
