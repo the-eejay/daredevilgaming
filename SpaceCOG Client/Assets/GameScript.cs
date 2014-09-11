dd6aae8f9baa333fc59d39b2ce3dcf856955070c
Geiun <alan.murtagh@gmail.com>
2014-09-11 11:50:50 +1000

    Merge branch 'networking'
    Conflicts:
    SpaceCOG Client/Library/CurrentLayout.dwlt
    SpaceCOG Client/Library/InspectorExpandedItems.asset
    SpaceCOG Client/Library/ScriptAssemblies/Assembly-CSharp.dll
    SpaceCOG Client/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    SpaceCOG Client/Library/assetDatabase3
    SpaceCOG Client/Library/expandedItems
    SpaceCOG Client/Library/guidmapper
    SpaceCOG Client/Library/metadata/03/0322d5b52c100c844be4713c8d79720b
    SpaceCOG Client/SpaceCOG Client-csharp.sln
    SpaceCOG Client/SpaceCOG Client.sln
    SpaceCOG Client/SpaceCOG Client.userprefs

---
+++ SpaceCOG Client/Assets/GameScript.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*	GameScript dictates all of the major game functionality. */
+public class GameScript : MonoBehaviour {
+
+	// GameObject Prefabs for cloning purposes.
+	private static GameObject shipPrefab;
+	private static GameObject bullet;
+	
+	// A specific reference to the player's ship.
+	private GameObject ship;
+	
+	// Misc mathematical constructs
+	private static Plane targettingPlane; // Used for projecting cursor coordinates to 3D space.
+	
+	// Ship stats & attributes
+	private const float thrust = 10000f; // Thrust applied to ship moving along axis.
+	private const float angleThrust = 7070f; // Thrust applied to ship moving diagonally.
+	
+	// Weapon stats & attributed
+	private const float bulletForce = 1000f; // Force applied to bullet when fired.
+
+	// Use this for initialization
+	void Start () {
+		// Object instantiation
+		targettingPlane = new Plane (Vector3.forward, Vector3.zero);
+		
+		// Load prefabs
+		shipPrefab = (GameObject) Resources.Load ("Magpie");
+		bullet = (GameObject) Resources.Load ("prefabBullet");
+		
+		// Spawn player ship
+		Debug.Log ("Game Loaded.");
+		ship = (GameObject) Network.Instantiate (shipPrefab, new Vector3 (-2f, 0f, 0f), Quaternion.identity, 0);
+		
+		// Launch master host script (if acting as host)
+		if (Network.isServer) {
+			gameObject.AddComponent ("HostScript");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Shoot ();
+	}
+	
+	// FixedUpdate is called at consistent time intervals.
+	// It is useful for physics effecting functionality, since
+	// the player's framerate won't impact the results.
+	void FixedUpdate () {
+		Move ();
+		Turn ();
+	}
+	
+	// SetInitialLocation allows the server to set the location of this player's ship.
+	[RPC] // This function can be called remotely over the network.
+	public void SetInitialLocation (float x, float y) {
+		ship.transform.localPosition = new Vector3 (x, y, 0f);
+	}
+	
+	// Turn to be called during FixedUpdate in order to orient the player's ship towards
+	// the player's cursor. Currently moves instantaneously, but the aim is to include
+	// turn-rates and torque / angular momentum to limit the rapidness of a turn.
+	void Turn () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		float dist = 2000f;
+		if (targettingPlane.Raycast(ray, out dist)) {
+			Vector3 targetCoordinates = ray.GetPoint(dist);
+			ship.transform.LookAt (targetCoordinates);
+		}
+	}
+	
+	// Shoot to be called during Update in order to spawn a bullet whenever the player
+	// clicks the left mouse button. Eventually this function will need to change to
+	// allow more advanced weapon types, and may need to be moved to FixedUpdate instead.
+	void Shoot() {
+		if (Input.GetMouseButtonDown(0)) {
+			GameObject tmp = (GameObject) Network.Instantiate (bullet, ship.transform.position, Quaternion.identity, 0);
+			Physics.IgnoreCollision(ship.collider, tmp.collider, true);
+			tmp.collider.enabled = true;
+			tmp.transform.position = ship.transform.position;
+			tmp.transform.rigidbody.velocity = ship.transform.rigidbody.velocity;
+			tmp.rigidbody.AddForce(ship.transform.forward * bulletForce);
+		}
+	}
+	
+	// Move to be called during FixedUpdate in order to control the player's ship.
+	// Uses force and momentum to move.
+	void Move () {
+		bool w = Input.GetKey ("w");
+		bool a = Input.GetKey ("a");
+		bool s = Input.GetKey ("s");
+		bool d = Input.GetKey ("d");
+		
+		// Cancel out opposing directions
+		if (w && s) {
+			w = false;
+			s = false;
+		}
+		if (a && d) {
+			a = false;
+			d = false;
+		}
+		
+		// Apply thrust
+		if (w && a)
+			ship.rigidbody.AddForce(new Vector3(-angleThrust, angleThrust, 0));
+		else if (w && d)
+			ship.rigidbody.AddForce(new Vector3(angleThrust, angleThrust, 0));
+		else if (w)
+			ship.rigidbody.AddForce(thrust * Vector3.up);
+		else if (a && s)
+			ship.rigidbody.AddForce(new Vector3(-angleThrust, -angleThrust, 0));
+		else if (a)
+			ship.rigidbody.AddForce(thrust * Vector3.left);
+		else if (s && d)
+			ship.rigidbody.AddForce(new Vector3(angleThrust, -angleThrust, 0));
+		else if (s)
+			ship.rigidbody.AddForce(thrust * Vector3.down);
+		else if (d)
+			ship.rigidbody.AddForce(thrust * Vector3.right);
+
+		// Move the player's camera to keep the ship centred.
+		Camera.main.transform.position = ship.transform.position - 10 * Vector3.forward;
+	}
+	
+	// Overriding MonoBehaviour method that is automatically called
+	// when this client is disconnected from the server.
+	private void OnDisconnectedFromServer (NetworkDisconnection info) {
+		Debug.Log ("Disconnected from the server: " + info.ToString ());
+		EndGame ();
+	}
+	
+	// Overriding MonoBehaviour method that is automatically called
+	// when a client disconnects from this server.
+	private void OnPlayerDisconnected (NetworkPlayer player) {
+		Debug.Log ("Player disconnected.");
+		EndGame ();
+	}
+	
+	// EndGame quits back to the main menu. 
+	private void EndGame () {
+		Application.LoadLevel ("Menu");
+	}
+
+}
---
+++ SpaceCOG Client/Assets/MainMenuScript.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*	MainMenuScript contains all functionality required by the main menu
+ *	scene in the game. It only needs to be attached to a single object
+ *	in the scene. Buttons can trigger public functions as necessary. */
+public class MainMenuScript : MonoBehaviour {
+
+	// These instance variables will be obsolete by the final release
+	private const int MAX_CONNECTIONS = 1;
+	private const int PORT_NO = 9001;
+	private const string HOST_IP = "127.0.0.1";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Function to be called when the 'Host' button is pressed.
+	// Begins listening for a connection attempt by another client.
+	// Shuts down server if clicked again.
+	public void HostServer () {
+		if (!Network.isServer) {
+			Debug.Log ("Attempting to host server...");
+			NetworkConnectionError e;
+			e = Network.InitializeServer (MAX_CONNECTIONS, PORT_NO, false);
+			
+			if (e == NetworkConnectionError.NoError) {
+				Debug.Log ("Listening for connections...");
+			} else {
+				Debug.Log ("Initialization Error: " + e.ToString ());
+			}
+		} else {
+			Debug.Log ("Shutting server down.");
+			Network.Disconnect (200);
+		}
+	}
+	
+	// Function to be called when the 'Join' button is pressed.
+	// Attempts to connect to a server on the local machine.
+	public void JoinServer () {
+		Debug.Log ("Attempting to connect to server...");
+		NetworkConnectionError e;
+		e = Network.Connect (HOST_IP, PORT_NO);
+		
+		if (e != NetworkConnectionError.NoError) {
+			Debug.Log ("Connection Error: " + e.ToString ());
+		}
+	}
+	
+	// Overriding MonoBehaviour method that is automatically called
+	// when a server is successfully initialized locally.
+	private void OnServerInitialized () {
+		Debug.Log ("Server successfully initialized.");
+	}
+	
+	// Overriding MonoBehaviour method that is automatically called
+	// when a client successfully connects to this server.
+	private void OnPlayerConnected (NetworkPlayer player) {
+		Debug.Log (player.ToString () + " has joined the server.");
+		Debug.Log ("Loading game...");
+		networkView.RPC ("Game", RPCMode.All);
+	}
+	
+	// Overriding MonoBehaviour method that is automatically called
+	// when this client successfully connects to a server.
+	private void OnConnectedToServer () {
+		Debug.Log ("Successfully connected to the server.");
+	}
+	
+	// Overriding MonoBehaviour method that is automatically called
+	// when this client is disconnected from the server.
+	private void OnDisconnectedFromServer (NetworkDisconnection info) {
+		Debug.Log ("Disconnected from the server: " + info.ToString ());
+	}
+	
+	// Overriding MonoBehaviour method that is automatically called
+	// when a client disconnects from this server.
+	private void OnPlayerDisconnected (NetworkPlayer player) {
+		Debug.Log ("Player disconnected.");
+	}
+	
+	// This function transitions the game into the actual gameplay.
+	[RPC] // This function can be called remotely over the network.
+	private void Game () {
+		Application.LoadLevel ("Game");
+	}
+	
+	// Function to be called when the 'Quit' button is pressed.
+	// Closes the program.
+	public void Quit () {
+		Debug.Log ("Application terminated by player.");
+		Application.Quit ();
+	}
+}
