1fc636e2d6f8a6cc3db89d958df0fa094d24e893
Scott Bilkey <swain_fan@hotmail.com>
2014-10-06 15:45:27 +1000

    Added asteroid belt

---
+++ SpaceCOG Client/Assets/Scripts/SpawnAsteroid.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnAsteroid : MonoBehaviour {
+	
+	public GameObject ObjectToSpawn; 
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 100; i++) {
+				Vector3 rndPosWithin;
+				rndPosWithin = new Vector3 (Random.Range (-1f, 1f), Random.Range (-1f, 1f), Random.Range (-1f, 1f));
+				rndPosWithin = transform.TransformPoint (rndPosWithin * .5f);
+				Instantiate (ObjectToSpawn, rndPosWithin, Random.rotation);  
+				Debug.Log ("spawn asteroids");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
