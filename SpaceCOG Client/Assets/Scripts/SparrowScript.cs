00d8edd94e59d07e012d5bb1011ff2fb73cb62a0
Ethan Christie <the.eejay@gmail.com>
2014-10-08 20:24:50 +1000

    Made bomber prefab

---
+++ SpaceCOG Client/Assets/Scripts/SparrowScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SparrowScript : enemyScript {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- SpaceCOG Client/Assets/Scripts/enemyScript.cs
+++ SpaceCOG Client/Assets/Scripts/enemyScript.cs
@@ -3,51 +3,43 @@ using System.Collections;
 
 public class enemyScript : MonoBehaviour {
 
+	public int bounty;
+	public bool canShoot;
 	public int speed;
-	public int score;
-	private GameScript gameController;
-	float duration = 10f;
-	float startTime;
+	public GameObject bullet;
 
 	// Use this for initialization
 	void Start () {
-		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
-		if (gameControllerObject != null) {
-			gameController = gameControllerObject.GetComponent<GameScript>();
-		}
-		if (gameController == null) {
-			Debug.Log ("Cannot find 'GameController' script");
-		}
 
-		Vector3 targetPos = gameController.GetPos ();
-		rigidbody.velocity = (targetPos - this.transform.position).normalized * speed;
-		
-		startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (Vector3.up * Time.deltaTime * 500);
-		this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, 0);
+		Move ();
+		Shoot();
+	}
+
+	void Move() {
+		GameObject target = GameObject.FindGameObjectWithTag ("Player");
+		Vector3 targetPos = target.rigidbody.position;
+		rigidbody.velocity = (targetPos - this.transform.position).normalized * speed;
+
+		Vector3 diff = target.transform.position - this.transform.position;
+		Vector3 dir = target.transform.position - this.transform.position;
+		dir.Normalize ();
 		
-		if (Time.time - startTime > duration) {
-			Network.Destroy (gameObject);
+		float rot = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg;
+		rot -= 90f;
+		this.transform.rotation = Quaternion.Euler (0f, 0f, rot);
+	}
+
+	void Shoot() {
+		if (canShoot) {
+			float rot = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg;
+			rot -= 90f;
+			baddies[i].transform.rotation = Quaternion.Euler (0f, 0f, rot);
 		}
 	}
 
-	void OnCollisionEnter (Collision col) {
-		if (col.gameObject.name == "prefabBullet(Clone)") {
-			// Asteroid collided with bullet
 
-			Network.Destroy(col.gameObject);
-			Network.Destroy (gameObject);
-			Destroy (this);
-			gameController.AddScore(score);
-		} else if (col.gameObject.name == "Magpie(Clone)") {
-			// Asteroid collided with player
-			// Network.Destroy (col.gameObject);
-			Network.Destroy (gameObject);
-			Destroy (this);
-		}
-	}
 }
