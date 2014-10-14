355e4fc386173c43f63cfb264f1a36d7f566cecf
Scott Bilkey <swain_fan@hotmail.com>
2014-10-14 17:25:51 +1000

    Added planet and fixed asteroid belt

--- SpaceCOG Client/Assets/Scripts/SpawnAsteroid.cs
+++ SpaceCOG Client/Assets/Scripts/SpawnAsteroid.cs
@@ -6,7 +6,7 @@ public class SpawnAsteroid : MonoBehaviour {
 	public GameObject ObjectToSpawn; 
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 1000; i++) {
 				Vector3 rndPosWithin;
 				rndPosWithin = new Vector3 (Random.Range (-1f, 1f), Random.Range (-1f, 1f), Random.Range (-1f, 1f));
 				rndPosWithin = transform.TransformPoint (rndPosWithin * .5f);
