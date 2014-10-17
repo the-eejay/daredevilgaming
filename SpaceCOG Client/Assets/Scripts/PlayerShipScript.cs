4e12dfa270ce1c944b0e7798519abafeea4764ad
Alan Murtagh <geiun@Mars.local>
2014-10-17 23:44:28 +1000

    Minor lag perception improvement
    At least I think so.

--- SpaceCOG Client/Assets/Scripts/PlayerShipScript.cs
+++ SpaceCOG Client/Assets/Scripts/PlayerShipScript.cs
@@ -15,6 +15,9 @@ public class PlayerShipScript : MonoBehaviour {
 
 	public string name;
 	public string description;
+	
+	public Vector3 cursor;
+	private static Plane targettingPlane;
 
 
 	// Use this for initialization
@@ -22,6 +25,7 @@ public class PlayerShipScript : MonoBehaviour {
 		if(master == null) {
 			master = GameObject.Find("WorldScriptObject");
 		}
+		targettingPlane = new Plane (Vector3.forward, Vector3.zero);
 	}
 	
 	// Update is called once per frame
@@ -31,11 +35,28 @@ public class PlayerShipScript : MonoBehaviour {
 		currency += Time.deltaTime;
 		PlayerPrefs.SetFloat("Money", currency);
 		PlayerPrefs.Save();
-
+		Move();
 	}
 	
 	void FixedUpdate () {
 	
+	}
+	
+	void Move () {
+		if (!Network.isServer && Network.isClient) {
+			// Cursor
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			float dist = 2000f;
+			if (targettingPlane.Raycast(ray, out dist)) {
+				cursor = ray.GetPoint(dist);
+			}
+		
+			Vector3 diff = cursor - this.transform.position;
+			diff.Normalize();
+			float rot = Mathf.Atan2 (diff.y, diff.x) * Mathf.Rad2Deg;
+			rot -= 90f;
+			this.transform.rotation = Quaternion.Euler (0f, 0f, rot);
+		}
 	}
 
 	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo SystemInfo) {
